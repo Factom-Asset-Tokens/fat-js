7355cbc36fed81fb6dd8fa27b5869ca808c59b0a
Devon Katz <devonk@dbgrow.com>
2018-10-16 19:55:53 -0700

    Add webpack, standardize entrypoint module.exports

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-module.exports = {
-    RPCBuilder: require('./rpc/RPC').RPCBuilder,
-    FAT0: {
-        RPCBuilder: require('./0/RPC'), //FAT-0 specific type casting
-        Transaction: require('./0/Transaction').Transaction,
-        TransactionBuilder: require('./0/Transaction').TransactionBuilder,
-        Issuance: require('./0/Issuance').Issuance,
-        IssuanceBuilder: require('./0/Issuance').IssuanceBuilder,
-    }
-};
+module.exports = Object.assign({},
+    require('./rpc/RPC'),
+);
