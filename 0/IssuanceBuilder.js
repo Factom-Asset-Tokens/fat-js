ab6112eea9991ab59d9bb8ca95b85757d99cc64d
Devon Katz <devonk@dbgrow.com>
2019-05-20 21:07:37 -0700

    FAT-0 Issuance: Move Issuance to own file, use Bignumber for supply

---
+++ 0/IssuanceBuilder.js
@@ -0,0 +1,90 @@
+const fctCryptoValidation = require('factom-identity-lib/src/validation');
+const fctIdentityUtil = require('factom-identity-lib/src/validation');
+const BigNumber = require('bignumber.js');
+const JSONBig = require('json-bigint')({strict: true});
+
+/**
+ * Build & Model A FAT-0 Issuance
+ * @alias Issuance0
+ * @public
+ * @class
+ */
+class IssuanceBuilder {
+
+    /**
+     * @constructor
+     * @param {string} tokenId - arbitrary string to use as a token identifier
+     * @param {string} rootChainId - 64 character Factom Chain ID of the identity to issue the token under
+     * @param {string} sk1 - SK1 Private key belonging to the identity at rootChainId
+     */
+    constructor(tokenId, rootChainId, sk1) {
+        if (!fctIdentityUtil.isValidIdentityChainId(rootChainId)) throw new Error("You must include a valid issuer identity Root Chain Id to issue a FAT token");
+        this._rootChainId = rootChainId;
+
+        if (tokenId === undefined || typeof tokenId !== 'string') throw new Error('Token is a required string');
+        this._tokenId = tokenId;
+
+        if (!fctCryptoValidation.isValidSk1(sk1)) throw new Error("Supplied key is not a valid sk1 private key");
+        this._sk1 = sk1;
+
+        this._type = 'FAT-0'
+    }
+
+    /**
+     * Set a symbol for the token
+     * @method
+     * @param {string} symbol - arbitrary string to use as a token symbol identifier. e.x. MYT
+     * @returns {IssuanceBuilder}
+     */
+    symbol(symbol) {
+        if (!symbol) throw new Error("Token symbol must be defined");
+        if (!new RegExp('[A-Z ]+').test(symbol)) throw new Error("Token symbol must only contain capital letters A-Z");
+        if (symbol.length == 0 || symbol.length > 4) throw new Error("Token symbol must be 1 - 4 characters");
+        this._symbol = symbol;
+        return this;
+    }
+
+    /**
+     * Set a maximum circulating supply for the token
+     * @method
+     * @param {number} supply - An integer maximum circulating supply to allow for the token. May be -1 for infinite, otherwise must be greater than 0
+     * @returns {IssuanceBuilder}
+     */
+    supply(supply) {
+        supply = new BigNumber(supply);
+        if (supply.isEqualTo(0) || supply.isLessThan(-1)) throw new Error("Supply must be equal to -1(infinite) or greater than 0");
+        this._supply = supply;
+        return this;
+    }
+
+    /**
+     * Set arbitrary metadata for the token issuance
+     * @method
+     * @param {*} metadata - The metadata. Must be JSON stringifyable
+     * @returns {IssuanceBuilder}
+     */
+    metadata(metadata) {
+        try {
+            JSONBig.stringify(metadata)
+        } catch (e) {
+            throw new Error("Transaction metadata bust be a valid JSON object or primitive");
+        }
+        this._metadata = metadata;
+        return this;
+    }
+
+    /**
+     * Build the issuance
+     * @method
+     * @returns {Issuance}
+     */
+    build() {
+        //validate required fields
+
+        if (this._supply === undefined) this._supply = -1; //unlimited supply by default
+
+        return new (require('./Issuance'))(this);
+    }
+}
+
+module.exports = IssuanceBuilder;
--- test/0/0.issuance.unit.spec.js
+++ test/0/0.issuance.unit.spec.js
@@ -6,7 +6,7 @@ const Chain = require('factom/src/chain').Chain;
 describe('Issuance Unit', function () {
 
     it('Builder', function () {
-        let IssuanceBuilder = require('../../0/Issuance').IssuanceBuilder;
+        let IssuanceBuilder = require('../../0/IssuanceBuilder');
         let issuance = new IssuanceBuilder("mytoken", "888888d027c59579fc47a6fc6c4a5c0409c7c39bc38a86cb5fc0069978493762", "sk11pz4AG9XgB1eNVkbppYAWsgyg7sftDXqBASsagKJqvVRKYodCU")
             .symbol('TTK')
             .supply(1000000)
@@ -16,7 +16,7 @@ describe('Issuance Unit', function () {
         assert.strictEqual(issuance.getIssuerIdentityRootChainId(), '888888d027c59579fc47a6fc6c4a5c0409c7c39bc38a86cb5fc0069978493762');
         assert.strictEqual(issuance.getType(), constant.FAT0);
         assert.strictEqual(issuance.getSymbol(), 'TTK');
-        assert.strictEqual(issuance.getSupply(), 1000000);
+        assert.isTrue(issuance.getSupply().isEqualTo(1000000));
         assert.strictEqual(JSON.stringify(issuance.getMetadata()), JSON.stringify({'abc': 123}));
 
         //Initial issuance specific
