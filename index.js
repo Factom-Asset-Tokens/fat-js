1f70f8ec5d9a928ad9cb9a3dc25e5a8515287aef
Devon Katz <devonk@dbgrow.com>
2019-05-21 20:13:14 -0700

    Expose IssuanceBuilder in index.js

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ module.exports = Object.assign({},
         FAT0: {
             TransactionBuilder: require('./0/TransactionBuilder'),
             Transaction: require('./0/Transaction'),
+            IssuanceBuilder: require('./0/IssuanceBuilder'),
             Issuance: require('./0/Issuance')
         },
         FAT1: {
             TransactionBuilder: require('./1/TransactionBuilder'),
             Transaction: require('./1/Transaction'),
+            IssuanceBuilder: require('./1/IssuanceBuilder'),
             Issuance: require('./1/Issuance')
         }
     }
